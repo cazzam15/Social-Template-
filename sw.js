2999c5c957121379ef0ad670263bb68bfcbadf24
cazzam15 <cmorrison79@msn.com>
2026-06-04 19:49:25 +0100

    Add PWA manifest, service worker, and icons

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'social-studio-v1';
+const ASSETS = [
+  '/Social-Template-/',
+  '/Social-Template-/index.html',
+  '/Social-Template-/manifest.json',
+  '/Social-Template-/icons/icon-192.png',
+  '/Social-Template-/icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
